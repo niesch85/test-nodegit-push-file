233b4a009048e5b9b9c6221040a3eb40b37f2419
niesch85 <niesch85@hotmail.com>
2022-11-04 20:49:34 +0100

    update db.json 04.11.22

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ async function executeGitCommands() {
 
 
 function execShellCommand(cmd) {
-    const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
